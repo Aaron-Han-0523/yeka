34d4b58654ec02734ab6d72859fb13d202c3168b
q7phavoc2 <q7phavoc2@gmail.com>
2022-12-09 11:27:10 +0900

    YEKA-013 John autoGenModel update & autoGenRepo add

---
+++ bin/auto_provider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:yeka/data/model/response/process_model.dart';
+import 'package:yeka/data/repository/process_repo.dart';
+
+import 'package:yeka/data/model/response/base/api_response.dart';
+
+class ProcessProvider extends ChangeNotifier {
+  final ProcessRepo processRepo;
+
+  ProcessProvider({@required this.processRepo});
+
+  ProcessModel _process;
+
+  ProcessModel get process => _process;
+
+  void addProcess(ProcessModel processModel) {
+    processRepo.addProcess(processModel);
+    notifyListeners();
+  }
+
+  void updateProcess(ProcessModel processModel) {
+    processRepo.updateProcess(processModel);
+    notifyListeners();
+  }
+
+  Future<ProcessModel> getProcess() async {
+    ApiResponse apiResponse = await processRepo.getProcess();
+    // apiResponse.response.data.forEach((process) => _process = ProcessModel.fromJson(process));
+    _process = ProcessModel.fromJson(apiResponse.response.data);
+    notifyListeners();
+    return _process;
+  }
+
+  Future<void> getProcessByCarNum(ProcessModel processModel) async {
+    ApiResponse apiResponse = await processRepo.getProcessByCarNum(processModel);
+    try {
+      apiResponse.response.data.forEach((process) => _process = ProcessModel.fromJson(process));
+    } catch (e) {
+      _process = null;
+    }
+    notifyListeners();
+  }
+}
