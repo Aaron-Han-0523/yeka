ad1f220aef11d0059ddfcf05826e3b582786afdb
aaronhan0523@gmail.com <aaronhan0523@gmail.com>
2023-02-08 11:14:00 +0900

    YEKA-066 Aaron 공지사항 코드정리

--- lib/view/screen/mypage/mypage_notice_board_detail_screen.dart
+++ lib/view/screen/mypage/mypage_notice_board_detail_screen.dart
@@ -6,7 +6,6 @@ import 'package:yeka/view/screen/home/widget/footer_screens.dart';
 import '../../../data/model/response/community_model.dart';
 import '../../../data/model/response/image_model.dart';
 import '../../../localization/language_constants.dart';
-import '../../../provider/auth_provider.dart';
 import '../../../provider/image_provider.dart';
 import '../../../util//images.dart';
 import '../../../util/app_constants.dart';
@@ -44,151 +43,156 @@ class _MyPageNoticeBoardDetailScreenState
   @override
   Widget build(BuildContext context) {
     return Consumer<CustomImageProvider>(
-        builder: (context, imageProvider, child) {
-          List<ImageModel> noticeImage = imageProvider.imageList;
-          noticeImage != null ? noticeImage : "";
-      return Scaffold(
+      builder: (context, imageProvider, child) {
+        List<ImageModel> noticeImage = imageProvider.imageList;
+        return Scaffold(
           resizeToAvoidBottomInset: false,
           body: SafeArea(
-            child: Stack(children: [
-              CustomScrollView(
-                controller: _scrollController,
-                slivers: [
-                  CustomSliverAppBar(
-                    "${getTranslated('NOTICE', context)}",
-                    isMyPageHidden: true,
-                  ),
-                  SliverToBoxAdapter(
-                    child: Column(
-                      children: [
-                        const Divider(
-                          height: 1,
-                          thickness: 1,
-                          indent: 0,
-                          endIndent: 0,
-                          color: Color(0xffDDDDDD),
-                        ),
-                        Container(
-                          color: Colors.white,
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 15, horizontal: 20),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      "${widget.communityModel != null ? widget.communityModel.community_title : ""}",
-                                      style: TextStyle(
-                                        fontSize: 12.0,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xff333333),
-                                        // overflow: TextOverflow.ellipsis,
+            child: Stack(
+              children: [
+                CustomScrollView(
+                  controller: _scrollController,
+                  slivers: [
+                    CustomSliverAppBar(
+                      "${getTranslated('NOTICE', context)}",
+                      isMyPageHidden: true,
+                    ),
+                    SliverToBoxAdapter(
+                      child: Column(
+                        children: [
+                          const Divider(
+                            height: 1,
+                            thickness: 1,
+                            indent: 0,
+                            endIndent: 0,
+                            color: Color(0xffDDDDDD),
+                          ),
+                          Container(
+                            color: Colors.white,
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 20),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${widget.communityModel != null ? widget.communityModel.community_title : ""}",
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xff333333),
+                                          // overflow: TextOverflow.ellipsis,
+                                        ),
+                                        textAlign: TextAlign.start,
                                       ),
-                                      textAlign: TextAlign.start,
-                                    ),
-                                    SizedBox(
-                                        height: Dimensions
-                                            .PADDING_SIZE_EXTRA_EXTRA_SMALL),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          "${widget.communityModel != null ? widget.communityModel.writer : ""}|${widget.communityModel != null ? widget.communityModel.create_date : ""}",
-                                          style: TextStyle(
-                                            fontSize: 8.0,
-                                            color: Color(0xff999999),
-                                            fontWeight: FontWeight.bold,
-                                            // overflow: TextOverflow.ellipsis,
+                                      SizedBox(
+                                          height: Dimensions
+                                              .PADDING_SIZE_EXTRA_EXTRA_SMALL),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            "${widget.communityModel != null ? widget.communityModel.writer : ""}|${widget.communityModel != null ? widget.communityModel.create_date : ""}",
+                                            style: TextStyle(
+                                              fontSize: 8.0,
+                                              color: Color(0xff999999),
+                                              fontWeight: FontWeight.bold,
+                                              // overflow: TextOverflow.ellipsis,
+                                            ),
                                           ),
-                                        ),
-                                        Row(
-                                          children: [
-                                            Image.asset(
-                                              Images.eye1,
-                                              fit: BoxFit.cover,
-                                              width: 10,
-                                            ),
-                                            SizedBox(width: 3),
-                                            Text(
-                                              "${widget.communityModel != null ? widget.communityModel.views : "0"}${getTranslated('TIMES', context)}",
-                                              style: TextStyle(
-                                                fontSize: 8.0,
-                                                color: Color(0xff999999),
-                                                fontWeight: FontWeight.bold,
-                                                // overflow: TextOverflow.ellipsis,
+                                          Row(
+                                            children: [
+                                              Image.asset(
+                                                Images.eye1,
+                                                fit: BoxFit.cover,
+                                                width: 10,
                                               ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
+                                              SizedBox(width: 3),
+                                              Text(
+                                                "${widget.communityModel != null ? widget.communityModel.views : "0"}${getTranslated('TIMES', context)}",
+                                                style: TextStyle(
+                                                  fontSize: 8.0,
+                                                  color: Color(0xff999999),
+                                                  fontWeight: FontWeight.bold,
+                                                  // overflow: TextOverflow.ellipsis,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const Divider(
+                                  height: 0,
+                                  thickness: 1,
+                                  indent: 20,
+                                  endIndent: 20,
+                                  color: Color(0xffDDDDDD),
+                                ),
+                                ClipRRect(
+                                  // borderRadius: BorderRadius.circular(10.0),
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 15, vertical: 15),
+                                    child: FadeInImage.assetNetwork(
+                                      placeholder: Images.placeholder1,
+                                      image: noticeImage.length != 0
+                                          ? AppConstants.BASE_URL +
+                                              "/" +
+                                              noticeImage[0].path
+                                          : AppConstants.BASE_URL +
+                                              "/placeholder_1x1.png",
+                                      fit: BoxFit.cover,
+                                      width: MediaQuery.of(context).size.width *
+                                          1.0,
+                                      height:
+                                          MediaQuery.of(context).size.width *
+                                              1.0,
+                                      imageErrorBuilder: (BuildContext context,
+                                          Object exception,
+                                          StackTrace stackTrace) {
+                                        return Container();
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 20),
+                                      child: Text(
+                                        "${widget.communityModel != null ? widget.communityModel.community_content : ""}",
+                                        style: TextStyle(
+                                            fontSize: 10,
+                                            color: Color(0xff333333)),
+                                      ),
                                     ),
                                   ],
                                 ),
-                              ),
-                              const Divider(
-                                height: 0,
-                                thickness: 1,
-                                indent: 20,
-                                endIndent: 20,
-                                color: Color(0xffDDDDDD),
-                              ),
-                              ClipRRect(
-                                // borderRadius: BorderRadius.circular(10.0),
-                                child: Container(
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 15, vertical: 15),
-                                  child: FadeInImage.assetNetwork(
-                                    placeholder: Images.placeholder1,
-                                    image: noticeImage.length != 0
-                                        ? AppConstants.BASE_URL +
-                                            "/" +
-                                            noticeImage[0].path
-                                        : AppConstants.BASE_URL +
-                                            "/placeholder_1x1.png",
-                                    fit: BoxFit.cover,
-                                    width: MediaQuery.of(context).size.width *
-                                        1.0,
-                                    height: MediaQuery.of(context).size.width *
-                                        1.0,
-                                    imageErrorBuilder: (BuildContext context,
-                                        Object exception,
-                                        StackTrace stackTrace) {
-                                      return Container();
-                                    },
-                                  ),
+                                SizedBox(
+                                  height: 125,
                                 ),
-                              ),
-                              Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 20),
-                                    child: Text(
-                                      "${widget.communityModel != null ? widget.communityModel.community_content : ""}",
-                                      style: TextStyle(
-                                          fontSize: 10,
-                                          color: Color(0xff333333)),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: 125,
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                        FooterPage(),
-                      ],
+                          FooterPage(),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ]),
-          ));
-    });
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
